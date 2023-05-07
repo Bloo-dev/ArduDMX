d7333ded45bb19b32bda786a67c899c185f261cf
Bloo-dev <contact.bluefire610@gmail.com>
2023-05-07 20:59:44 +0200

    Fix missing "," in test-LatchedButton

--- software/diagnostics/test-LatchedButton.ino
+++ software/diagnostics/test-LatchedButton.ino
@@ -2,7 +2,7 @@
 
 int FRAME_TIME_MS = 10;
 LatchedButton<8> buttonPlus(3, 1000 / FRAME_TIME_MS);
-LatchedButton<8> buttonSelect(5 1000 / FRAME_TIME_MS);
+LatchedButton<8> buttonSelect(5, 1000 / FRAME_TIME_MS);
 LatchedButton<8> buttonMinus(6, 1000 / FRAME_TIME_MS);
 LatchedButton<8> buttonFunction(9, 1000 / FRAME_TIME_MS);
 
